3eb04bfaf072a9c2a3bd7674ad5e284a0adbd41e
WizzOFF <maher_off@proton.me>
2025-04-21 21:39:11 +0200

    calculatrice.hpp: create class

---
+++ Calculatrice.hpp
@@ -0,0 +1,14 @@
+
+#ifdef calculatrice_hpp
+#define calculatrice_hpp
+
+#include <iosream>
+#include <vector>
+
+Class Calculatrice
+{
+    public:
+        static int somme(std::vector<int> vector);
+};
+
+#endif /* Calculatric_hpp */
